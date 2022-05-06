8353c488a8f40d09f5cc968abb4b95132744a32d
Sabina A. Salehova <sabinaas@code.local>
2022-05-06 21:48:55 +0400

    added task 06.05.2022

---
+++ 06.05.2022/task/task/Gun.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace task
+{
+    class Gun
+    {
+        public string type;
+        public int TotalBulletSize=90;
+        public static int MagazineSize=10;
+        public int BulletLeft=0;
+
+
+        public void shoot()
+        {
+                      
+            if (MagazineSize == BulletLeft)
+            {
+                Console.WriteLine(" Magazine Size is empty, please reload");
+                return;
+            }
+
+            BulletLeft++;                     
+        }
+
+        public void reload()
+        {
+            if (TotalBulletSize <= 0)
+            {
+                Console.WriteLine(" Total Bullet Size is empty");
+                return;
+            }
+            if (TotalBulletSize > BulletLeft)
+            {
+                TotalBulletSize = TotalBulletSize - BulletLeft;
+                BulletLeft = 0;
+            }
+            else
+            {
+                BulletLeft -= TotalBulletSize;
+                TotalBulletSize = 0;
+            }
+        }
+
+        public void fullShoot()
+        {
+            if (BulletLeft == 0)
+            {
+                Console.WriteLine(" Total Bullet Size is empty, before reload");
+                return;
+            }
+            TotalBulletSize -= BulletLeft;
+            BulletLeft = MagazineSize;
+
+
+        }
+
+    }
+}
---
+++ 06.05.2022/task/task/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Gun gun1 = new Gun();
+            Console.WriteLine(" Magazine size: " + Gun.MagazineSize + "\n Total Bullet Size: " + gun1.TotalBulletSize + "\n Bullet Left: " + gun1.BulletLeft);
+            Console.WriteLine("\n 1. Shoot \n 2. Reload \n 3. FullShoot \n 4. Exit program");
+            Console.Write("\n Please enter prosses: ");
+            int answer = Convert.ToInt32(Console.ReadLine());      
+            
+            do
+            {
+                switch (answer)
+                {
+                    case 1:
+                        gun1.shoot();
+                        break;
+                    case 2:
+                        gun1.reload();
+                        break;
+                    case 3:
+                        gun1.fullShoot();
+                        break;
+                }
+                Console.Write("\n Please enter prosses: ");
+                answer = Convert.ToInt32(Console.ReadLine());
+            } while (answer != 4);
+
+            Console.WriteLine("\n Magazine size: "+Gun.MagazineSize+ "\n Total Bullet Size: "+gun1.TotalBulletSize+ "\n Bullet Left: "+gun1.BulletLeft);
+        }
+    }
+}
